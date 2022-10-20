9ac4530bb3eae5742cdf794b032f3f743c869ecb
ddeexxiikk <jakubbatycki@protonmail.ch>
2022-10-20 21:14:39 +0200

    Dodałem funkcję, które korzystają z połączenia z bazą danych. Zrobiłem również zarys ich ciał

--- PolaczenieZBazaSQL.cpp
+++ PolaczenieZBazaSQL.cpp
@@ -1,19 +1,19 @@
 #include "PolaczenieZBazaSQL.h"
-#include <iostream>
+#include "Czlowiek.h"
 #include <string>
 
 using namespace std;
 
-void PolaczenieZBazaSQL()
+bool PolaczenieZBazaSQL()
 {
     //Proces Łączenia z Bazą SQL
     //Trzeba go zrobić
 
-    //Jeśli się udało
-    cout << "Polaczono z baza danych" << endl;
-
-    //Jeśli się nie udało
-    cout << "Brak polaczenia z baza danych" << endl;
+    bool proces;
+    if(proces) // Jeśli się udało
+        return true;
+    else //Jeśli się nie udało
+        return false;
 };
 
 string LoginZBazySQL()
@@ -22,16 +22,18 @@ string LoginZBazySQL()
     //Proces Pobierania Loginu z Bazy SQL
     //Trzeba go zrobić
 
-    bool Proces;
+    bool Proces = PolaczenieZBazaSQL();
 
     if(Proces) //Jeśli się udało
     {
+        //Zrobić wyciagniecie loginu z Bazy SQL i zapisanie go do zmiennej PoprawnyLogin
+
         PoprawnyLogin = "Login";
         return PoprawnyLogin;
     }
     else //Jeśli się nie udało
     {
-        PoprawnyLogin = "BlednyLogin";
+        PoprawnyLogin = nullptr;
         return PoprawnyLogin;
     }
 };
@@ -42,16 +44,230 @@ string HasloZBazySQL()
     //Proces Pobierania Hasła z Bazy SQL
     //Trzeba go zrobić
 
-    bool Proces;
+    bool Proces = PolaczenieZBazaSQL();
 
     if(Proces) //Jeśli się udało
     {
+        //Zrobić wyciagniecie hasla z Bazy SQL i zapisanie go do zmiennej PoprawneHaslo
+
         PoprawneHaslo = "Login";
         return PoprawneHaslo;
     }
     else //Jeśli się nie udało
     {
-        PoprawneHaslo = "BlednyLogin";
+        PoprawneHaslo = nullptr;
         return PoprawneHaslo;
     }
+};
+
+bool ZmianaHaslaPrzezStudenta()
+{
+    //Proces Zmiany Hasła przez Studenta
+    //Trzeba go zrobić
+
+    bool proces;
+    if(proces) // Jeśli się udało
+        return true;
+    else //Jeśli się nie udało
+        return false;
+};
+
+bool ZmianaHaslaPrzezPracownika()
+{
+    //Proces Zmiany Hasła Studenta lub własnego przez Pracownika
+    //Trzeba go zrobić
+
+    bool proces;
+    if(proces) // Jeśli się udało
+        return true;
+    else //Jeśli się nie udało
+        return false;
+};
+
+bool ZmianaHaslaPrzezAdmina()
+{
+    //Proces Zmiany Hasła Studenta, Pracownika lub własnego przez Admina
+    //Trzeba go zrobić
+
+    bool proces;
+    if(proces) // Jeśli się udało
+        return true;
+    else //Jeśli się nie udało
+        return false;
+};
+
+string PokazNumerStudenta()
+{
+    string NumerStudenta;
+    //Proces Pobierania Numeru Studenta z Bazy SQL
+    //Trzeba go zrobić
+
+    bool Proces = PolaczenieZBazaSQL();
+
+    if(Proces) //Jeśli się udało
+    {
+        //Zrobić wyciagniecie numeru studenta z Bazy SQL i zapisanie go do zmiennej NumerStudenta
+
+        NumerStudenta = "NumerStudenta";
+        return NumerStudenta;
+    }
+    else //Jeśli się nie udało
+    {
+        NumerStudenta = nullptr;
+        return NumerStudenta;
+    }
+};
+
+string PokazNumerStudentaPracownik(string aImie, string aNazwisko)
+{
+    string NumerStudenta;
+    //Proces Pobierania Numeru Studenta z Bazy SQL
+    //Trzeba go zrobić
+
+    bool Proces = PolaczenieZBazaSQL();
+
+    if(Proces) //Jeśli się udało
+    {
+        //Zrobić wyciagniecie numeru studenta z Bazy SQL i zapisanie go do zmiennej NumerStudenta
+
+        NumerStudenta = "NumerStudenta";
+        return NumerStudenta;
+    }
+    else //Jeśli się nie udało
+    {
+        NumerStudenta = nullptr;
+        return NumerStudenta;
+    }
+};
+
+bool DodanieStudenta()
+{
+    //Proces Dodania Studenta do Bazy SQL
+    //Trzeba go zrobić
+
+    bool proces;
+    if(proces) // Jeśli się udało
+        return true;
+    else //Jeśli się nie udało
+        return false;
+};
+
+bool UsuniecieStudenta(string NrStudenta)
+{
+    //Proces Usunięcia Studenta z Bazy SQL
+    //Trzeba go zrobić
+
+    bool proces;
+    if(proces) // Jeśli się udało
+        return true;
+    else //Jeśli się nie udało
+        return false;
+};
+
+bool DodaniePracownika()
+{
+    //Proces Dodania Pracownika do Bazy SQL
+    //Trzeba go zrobić
+
+    bool proces;
+    if(proces) // Jeśli się udało
+        return true;
+    else //Jeśli się nie udało
+        return false;
+};
+
+void ZobaczenieOcenStudent()
+{
+    //Proces Zobaczenia Ocen Studenta
+    //Trzeba go zrobić
+};
+
+void ZobaczenieOcenPracownik(string NrStudenta)
+{
+    //Proces Zobaczenia Ocen Studenta
+    //Trzeba go zrobić
+};
+
+bool DodanieOceny(string NrStudenta)
+{
+    //Proces Dodania Oceny Studenta
+    //Trzeba go zrobić
+
+    bool proces;
+    if(proces) // Jeśli się udało
+        return true;
+    else //Jeśli się nie udało
+        return false;
+};
+
+void SprawdzenieGrupyStudent()
+{
+    //Proces Sprawdzenia Grupy Studenta
+    //Trzeba go zrobić
+};
+
+void SprawdzenieGrupyPracownik(string NrStudenta)
+{
+    //Proces Sprawdzenia Grupy Studenta
+    //Trzeba go zrobić
+};
+
+void SprawdzenieKsiazekStudent()
+{
+    //Proces Sprawdzenia Książek Studenta
+    //Trzeba go zrobić
+};
+
+void SprawdzenieKsiazekPracownik(string NrStudenta)
+{
+    //Proces Sprawdzenia Książek Studenta
+    //Trzeba go zrobić
+};
+
+void SprawdzeniePlanuZajec()
+{
+    //Proces Sprawdzenia Planu Zajęć
+    //Trzeba go zrobić
+};
+
+void SprawdzeniePlanuZajecPracownik(string NrStudenta)
+{
+    //Proces Sprawdzenia Planu Zajęć
+    //Trzeba go zrobić
+};
+
+void PrzegladniecieOgloszenStudent()
+{
+    //Proces Przeglądnięcia Ogłoszeń
+    //Trzeba go zrobić
+};
+
+void PrzegladniecieOgloszenPracownik()
+{
+    //Proces Przeglądnięcia Ogłoszeń
+    //Trzeba go zrobić
+};
+
+bool DodanieOgloszen()
+{
+    //Proces Dodania Ogłoszenia
+    //Trzeba go zrobić
+
+    bool proces;
+    if(proces) // Jeśli się udało
+        return true;
+    else //Jeśli się nie udało
+        return false;
+};
+
+bool UsuniecieOgloszen(string NrOgloszenia)
+{
+    //Proces Usunięcia Ogłoszenia
+    //Trzeba go zrobić
+
+    bool proces;
+    if(proces) // Jeśli się udało
+        return true;
+    else //Jeśli się nie udało
+        return false;
 };
--- PolaczenieZBazaSQL.h
+++ PolaczenieZBazaSQL.h
@@ -1,12 +1,44 @@
 #ifndef WIRTUALNYDZIEKANAT_POLACZENIEZBAZASQL_H
 #define WIRTUALNYDZIEKANAT_POLACZENIEZBAZASQL_H
+#include "Czlowiek.h"
 #include <string>
 
 using namespace std;
 
-void PolaczenieZBazaSQL();
+string LoginDoBazyDanych, HasloDoBazyDanych;
 
 string LoginZBazySQL();
 string HasloZBazySQL();
+
+bool PolaczenieZBazaSQL();
+
+bool ZmianaHaslaPrzezStudenta();
+bool ZmianaHaslaPrzezPracownika();
+bool ZmianaHaslaPrzezAdmina();
+
+string PokazNumerStudenta();
+string PokazNumerStudentaPracownik(string aImie, string aNazwisko);
+
+bool DodanieStudenta();
+bool UsuniecieStudenta(string NrStudenta);
+bool DodaniePracownika();
+
+void ZobaczenieOcenStudent();
+void ZobaczenieOcenPracownik(string NrStudenta);
+bool DodanieOceny(string NrStudenta);
+
+void SprawdzenieGrupyStudent();
+void SprawdzenieGrupyPracownik(string NrStudenta);
+
+void SprawdzenieKsiazekStudent();
+void SprawdzenieKsiazekPracownik(string NrStudenta);
+
+void SprawdzeniePlanuZajec();
+void SprawdzeniePlanuZajecPracownik(string NrStudenta);
+
+void PrzegladniecieOgloszenStudent();
+void PrzegladniecieOgloszenPracownik();
+bool DodanieOgloszen();
+bool UsuniecieOgloszen(string NrOgloszenia);
 
 #endif
