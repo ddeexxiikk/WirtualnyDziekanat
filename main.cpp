0b170706f924b35459e462f0388f050abbb410aa
ddeexxiikk <jakubbatycki@protonmail.ch>
2022-10-20 21:13:09 +0200

    Dodałem pliki nagłówkowe i biblioteki używane, zrobiłem etap logowania w main'ie

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include "Czlowiek.h"
-#include "PanelLogowania.h"
 #include "PolaczenieZBazaSQL.h"
+#include "PanelLogowania.h"
+#include "Czlowiek.h"
+#include <windows.h>
 #include <iostream>
 #include <string>
 
@@ -9,6 +10,22 @@ using namespace std;
 int main()
 {
     cout << "Witaj w Wirtualnym Dziekanacie" << endl;
-  
+    cout << "Zaloguj sie aby przejsc dalej..." << endl;
+
+    //Proces logowania
+    if(PanelLogowania())
+    {
+        //Uzytkownik jest poprawny i czekamy 1s
+        cout << "Zalogowano poprawnie,\nZaraz przejdziemy dalej..." << endl;
+        Sleep(1000);
+    }
+    else
+        //Jeśli użytkownik więcej niż 3 pomyli się przy logowaniu, program się wyłączy
+        exit(0);
+
+    //Od tego momentu możemy przejść do profilu Studenta/Pracownika
+    //Mozecie tutaj wrzucic funkcje profilow
+    //W tym momencie jestem w stanie tylko sprawdzic czy dziala logowanie na sztywne dane, bo jeszcze nie stworzyłem bazy danych
+
     return 0;
 }
