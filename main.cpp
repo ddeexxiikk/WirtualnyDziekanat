61d8a94139e5b4f4a51fa2317f9d82d2a80b058d
ddeexxiikk <jakubbatycki@protonmail.ch>
2022-12-15 15:30:18 +0100

    Nic znaczącego

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "Panel.h"
-#include <windows.h>
-#include <iostream>
-#include <fstream>
-#include <string>
 #include "Student.h"
 #include "Pracownik.h"
 #include "Plan_zajec.h"
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -13,7 +13,7 @@ int main()
 {
     cout << "Witaj w Wirtualnym Dziekanacie" << endl;
     cout << "Zaloguj sie aby przejsc dalej..." << endl;
-    void *obiekt;
+    void * obiekt;
     int poziom_dostepu=PanelLogowania(obiekt);
     if(poziom_dostepu==1) ((Student*)obiekt)->display();
     if(poziom_dostepu==2) ((Pracownik*)obiekt)->display();
